20c40287cc9c5e7d7ada8225b8f772dbae1e6e55
cinmon <150573808+cinmon@users.noreply.github.com>
2025-04-02 12:32:49 +0900

    add streamlit app

---
+++ app.py
@@ -0,0 +1,35 @@
+import streamlit as st
+from inference import load_model, predict_image
+import os
+
+st.set_page_config(page_title="Dog Breed Classifier", page_icon="🐶", layout="centered")
+st.title("🐶 Dog Breed Classifier")
+
+uploaded_file = st.file_uploader("Upload an image of a dog!", type=["jpg", "jpeg", "png"])
+
+if uploaded_file is not None:
+
+    # Save the uploaded file to a temporary location
+    st.image(uploaded_file, caption="Uploaded Image", use_column_width=True)
+    st.write("Classifying...")
+
+    temp_file_path = "temp_image.jpg"
+    with open(temp_file_path, "wb") as f:
+        # f.write(uploaded_file.getbuffer())
+        f.write(uploaded_file.read())
+
+    # Load the model
+    model = load_model()
+
+    st.write("Model loaded")
+    st.write("Predicting...")
+
+    # Predict the breed of the dog in the uploaded image
+    predicted_class, confidence = predict_image(temp_file_path, model)
+
+    # Display the results
+    st.image(uploaded_file, caption="Uploaded Image", use_column_width=True)
+    st.success(f"Prediction: **{predicted_class}** ({confidence * 100:.2f}% confident)")
+
+    # Clean up the temporary file
+    os.remove(temp_file_path)
